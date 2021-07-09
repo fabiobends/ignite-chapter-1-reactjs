e54b65bf5616676f4a7ea663a1ec84f3fa1cbf2a
Fábio Mendes <fabiobends@gmail.com>
2021-07-09 20:34:00 -0300

    add: static HTML server and webpack server

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 export function App() {
-  return <h1>hey</h1>
+  return <h1>hello react</h1>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 	mode: 'development',
@@ -7,6 +8,14 @@ module.exports = {
 	resolve: {
 		extensions: ['.js', '.jsx']
 	},
+	devServer: {
+		contentBase: path.resolve(__dirname, 'public'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public', 'index.html')
+		})
+	],
 	module: {
 		rules: [
 			{
