dfb2a2672b0882425f12bd6d94c922bc0d1c69b2
Fábio Mendes <fabiobends@gmail.com>
2021-07-09 20:58:12 -0300

    add: environment modes and style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const isProductionMode = process.env.NODE_ENV === 'production'
+
 module.exports = {
-	mode: 'development',
+	mode: isProductionMode ? 'production' : 'development',
+	devtool: isProductionMode ? 'source-map' : 'eval-source-map',
 	entry: path.resolve(__dirname, 'src', 'index.jsx'),
 	output: { path: path.resolve(__dirname, 'dist'), filename: 'bundle.js' },
 	resolve: {
@@ -22,6 +25,16 @@ module.exports = {
 				test: /\.jsx$/,
 				exclude: /node_modules/,
 				use: 'babel-loader'
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
 			}
 		]
 	}
