4027784c846e8f966dd33838c51fea495b22bb23
Fábio Mendes <fabiobends@gmail.com>
2021-07-12 23:23:06 -0300

    add: react components, immutability and fast refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,51 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
-const isProductionMode = process.env.NODE_ENV === 'production'
+const isProductionMode = process.env.NODE_ENV === "production";
 
 module.exports = {
-	mode: isProductionMode ? 'production' : 'development',
-	devtool: isProductionMode ? 'source-map' : 'eval-source-map',
-	entry: path.resolve(__dirname, 'src', 'index.jsx'),
-	output: { path: path.resolve(__dirname, 'dist'), filename: 'bundle.js' },
-	resolve: {
-		extensions: ['.js', '.jsx']
-	},
-	devServer: {
-		contentBase: path.resolve(__dirname, 'public'),
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, 'public', 'index.html')
-		})
-	],
-	module: {
-		rules: [
-			{
-				test: /\.jsx$/,
-				exclude: /node_modules/,
-				use: 'babel-loader'
-			},
-			{
-				test: /\.css$/,
-				exclude: /node_modules/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.scss$/,
-				exclude: /node_modules/,
-				use: ['style-loader', 'css-loader', 'sass-loader']
-			}
-		]
-	}
-}
+  mode: isProductionMode ? "production" : "development",
+  devtool: isProductionMode ? "source-map" : "eval-source-map",
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: { path: path.resolve(__dirname, "dist"), filename: "bundle.js" },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+    hot: true,
+  },
+  plugins: [
+    !isProductionMode && new ReactRefreshWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              !isProductionMode && require.resolve("react-refresh/babel"),
+            ].filter(Boolean),
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
